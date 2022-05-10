8fbb4fd07ef0037072fa05e8636189f9dd8c336c
ayan-l <ayanlaila1@gmail.com>
2022-05-10 11:44:06 -0700

    new status

--- src/GitHubClassPractice.java
+++ src/GitHubClassPractice.java
@@ -2,7 +2,11 @@
 public class GitHubClassPractice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int a = 10;
+		int b = 20;
+		int c = a + b;
+		System.out.println(c);
 
 	}
 
